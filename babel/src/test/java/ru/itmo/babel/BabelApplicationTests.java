d5166892d43edacc14593357fbd66539978168e5
Jegor Popow <juicedogegor@gmail.com>
2025-10-30 02:49:54 +0300

    Add template

---
+++ babel/src/test/java/ru/itmo/babel/BabelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.babel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
