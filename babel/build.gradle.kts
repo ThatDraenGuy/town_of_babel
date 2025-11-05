39f501e6bffc8ce5dbc4c149b550720ad715f057
Oleg Shipulin <olegshipu95@gmail.com>
2025-11-05 16:07:31 +0300

    feat: add support for H2 and Postgres

--- babel/build.gradle.kts
+++ babel/build.gradle.kts
@@ -29,8 +29,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
+
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+
+
+    compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
