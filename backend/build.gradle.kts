a25c925df376c8d2d86b089f89971ba4f835c777
Oleg Shipulin <olegshipu95@gmail.com>
2025-11-21 20:05:35 +0300

    Add api-docs.yaml and swagger

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
     runtimeOnly("com.h2database:h2")
